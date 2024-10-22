e6a8e612fe9314ffe30f1c4aeeb44e90fb94137a
camly0911 <lyntc@runsystem.net>
2024-10-22 15:32:57 +0700

    change icon be cap

--- app/models/be.rb
+++ app/models/be.rb
@@ -1,9 +1,10 @@
 class Be < ApplicationRecord
   geocoded_by :address
   # after_validation :geocode, if: :address_changed?
-  validates :loaibe, presence: true
-  validates :coquan, presence: true
-
+  validates :loaibe, presence: true, presence: { message: 'Không được để trống loại bể.' }
+  validates :coquan, presence: true, presence: { message: 'Không được để trống đơn vị quản lý.'}
+  validates :latitude, presence: true, presence: { message: 'Không được để trống vĩ độ.'}
+  validates :longitude, presence: true, presence: { message: 'Không được để trống kinh độ.'}
   has_many :duong_ongs_dau, class_name: 'DuongOng', foreign_key: 'be_dau_id'
   has_many :duong_ongs_cuoi, class_name: 'DuongOng', foreign_key: 'be_cuoi_id'
 
