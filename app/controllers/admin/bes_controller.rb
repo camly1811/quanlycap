78239b06d08138941b5854fc5df5164740ac6c4d
camly0911 <lyntc@runsystem.net>
2024-12-12 11:34:42 +0700

    create edit bes and icon delete

--- app/controllers/admin/bes_controller.rb
+++ app/controllers/admin/bes_controller.rb
@@ -19,5 +19,26 @@ module Admin
       @tong = @donvql.values.sum
       @donvql_tong = @donvql.transform_values { |v| (v.to_f / @tong * 100).round(2) }
     end
+
+    # Action để hiển thị form chỉnh sửa thông tin bể cáp
+    def edit
+      @be = Be.find(params[:id])
+    end
+    # Action để cập nhật thông tin bể cáp sau khi chỉnh sửa
+    def update
+      @be = Be.find(params[:id])
+      if @be.update(be_params)
+        redirect_to admin_bes_path, notice: 'Thông tin bể cáp đã được cập nhật thành công.'
+      else
+        render :edit
+      end
+    end
+    
+    private
+
+    # Dùng strong parameters để bảo mật dữ liệu
+    def be_params
+      params.require(:be).permit(:address, :tenbe, :loaibe, :coquan, :latitude, :longitude)
+    end
   end
 end
