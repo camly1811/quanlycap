57b786fca881c35b7899e5fad7688969f51dc418
camly0911 <lyntc@runsystem.net>
2024-12-13 08:13:10 +0700

    Sign in admin and user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[role_ids first_name last_name phone address gender])  
   end
+
+    # Ghi đè logic sau khi người dùng đăng nhập
+    def after_sign_in_path_for(resource)
+      # Kiểm tra vai trò người dùng
+      if resource.admin?
+        admin_bes_path
+      else
+        root_path
+      end
+    end
 end
